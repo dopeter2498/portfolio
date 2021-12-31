f159807b5872777f3a1a7b9dd85857dd57c7ef3a
Peter <dopeter2498@gmail.com>
2021-12-31 03:11:18 -0800

    Reverting to the state of c0601e1

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import '../styles/navbar.css';
+export default function Navbar(){
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="https://github.com/dopeter2498">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/peter-do-b0640b149/">LinkedIn</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+        );
+}
